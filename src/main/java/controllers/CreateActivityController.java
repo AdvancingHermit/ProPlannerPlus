4d57d64ea00d3cc727e3592bd396dbc6c9ee00e5
lolOzzi <47964440+lolOzzi@users.noreply.github.com>
2024-05-03 12:26:29 +0200

    Create Activity UI update

--- src/main/java/controllers/CreateActivityController.java
+++ src/main/java/controllers/CreateActivityController.java
@@ -1,13 +1,19 @@
 package controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import model.Activity;
+import model.DataModel;
+import model.Employee;
 import model.Project;
 
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.List;
 
 public class CreateActivityController extends StandardController {
 
@@ -19,6 +25,16 @@ public class CreateActivityController extends StandardController {
     private DatePicker createActivityStart;
     @FXML
     private DatePicker createActivityEnd;
+
+    @FXML
+    ComboBox<String> projectSelector;
+
+    public void initController(DataModel model, ProPlannerPlus proPlannerPlus) {
+        super.initController(model, proPlannerPlus);
+        List<String> projectNames = proPlannerPlus.getProjects().stream().map(Project::getName).toList();
+        ObservableList<String> projects = FXCollections.observableList(projectNames);
+        projectSelector.setItems(projects);
+    }
 
     @FXML
     private void switchToHome() throws IOException {
