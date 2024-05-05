e34fe158786d4b9e1ba45917c376f40a2e356608
lolOzzi <47964440+lolOzzi@users.noreply.github.com>
2024-05-05 22:35:32 +0200

    removed uneeded imports

--- src/main/java/controllers/ViewProjectsController.java
+++ src/main/java/controllers/ViewProjectsController.java
@@ -1,21 +1,15 @@
 package controllers;
 
-import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
-import model.Activity;
 import model.DataModel;
 import model.Employee;
 import model.Project;
 
 import java.io.IOException;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Map;
 
 public class ViewProjectsController extends StandardController {
 
