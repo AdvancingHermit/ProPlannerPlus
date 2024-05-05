893fbddc2d4134028d19b23b149db7e414406562
lolOzzi <47964440+lolOzzi@users.noreply.github.com>
2024-05-05 13:24:43 +0200

    updated getCompletionStatus

--- src/main/java/controllers/ProPlannerPlus.java
+++ src/main/java/controllers/ProPlannerPlus.java
@@ -240,8 +240,8 @@ public class ProPlannerPlus {
         double sumUsed = 0;
 
         for (int id : project.getActivityIDs()){                            //3
-            if(getActivity(id).getComplete()) {
-                for (Employee employee : getActivity(id).getEmployees()) {  //4
+            if(getActivity(id).getComplete()) {                             //4
+                for (Employee employee : getActivity(id).getEmployees()) {  //5
                     sumTotal += employee.getTimeUsed(id);
                     sumUsed += employee.getTimeUsed(id);
                 }
@@ -249,7 +249,10 @@ public class ProPlannerPlus {
                 sumTotal += getActivity(id).getTotalTime();
             }
         }
-        return sumTotal / sumUsed;                                          //5
+        if (sumUsed == 0) {                                                 //6
+            return 0;
+        }
+        return sumTotal / sumUsed;                                          //7
     }
 
 
