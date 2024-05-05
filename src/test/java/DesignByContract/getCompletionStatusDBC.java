067135a12eb4d6069e7e797b4518f0c558cf1557
lolOzzi <47964440+lolOzzi@users.noreply.github.com>
2024-05-06 00:09:23 +0200

    Update getCompletionStatusDBC.java

--- src/test/java/DesignByContract/getCompletionStatusDBC.java
+++ src/test/java/DesignByContract/getCompletionStatusDBC.java
@@ -13,10 +13,12 @@ import java.time.LocalDate;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class getCompletionStatusDBC {
     private ProPlannerPlus proPlannerPlus;
     private Employee employee;
-
+    private Project emptyProject;
     private Project project;
     private Activity activity;
 
@@ -25,27 +27,36 @@ public class getCompletionStatusDBC {
     @BeforeEach
     void setUp() throws OperationNotAllowedException {
         proPlannerPlus = new ProPlannerPlus();
-        project = new Project("testProj",1);
+        project = new Project("project",1);
+        emptyProject= new Project("empty",1);
         employee = new Employee("test");
         proPlannerPlus.createTestProject(project);
         proPlannerPlus.clearEmployees();
         proPlannerPlus.addEmployee(employee);
-        proPlannerPlus.createActivity("testAct", 10, LocalDate.of(2024,05,03),
-                LocalDate.of(2024,05,04), project.getId());
-        proPlannerPlus.addActivityToProject(project,proPlannerPlus.getActivities().get(0).getActivityID());
-        proPlannerPlus.addEmployeeDirectlyToActivity(proPlannerPlus.getActivities().get(0).getActivityID(), employee);
 
-        startDate =  LocalDate.of(2024,05,03);
-        endDate =  LocalDate.of(2024,05,04);
+        proPlannerPlus.createActivity("testAct", 10, LocalDate.of(2024,05,03),
+                LocalDate.of(2024,05,04), project.getId(), 10);
+        proPlannerPlus.addActivityToProject(project,proPlannerPlus.getActivities().get(0).getActivityID());
+
+        proPlannerPlus.addEmployeeDirectlyToActivity(proPlannerPlus.getActivities().get(0).getActivityID(), employee);
+        employee.registerTime(10, 1);
 
     }
 
     @Test
-    void testValidGetFreeEmployees() throws OperationNotAllowedException {
-        Map<Employee, Integer> expectedFreeEmployees = new LinkedHashMap<>();
-        expectedFreeEmployees.put(employee, 1);
-        Map<Employee, Integer> freeEmployees = proPlannerPlus.getFreeEmployees(startDate, endDate);
-
-        Assertions.assertEquals(expectedFreeEmployees, freeEmployees);
+    void testProjectNull() {
+        AssertionError assertionError = assertThrows(AssertionError.class, () -> proPlannerPlus.getCompletionStatus(null));
+        Assertions.assertEquals("Project cannot be null", assertionError.getMessage());
     }
+    @Test
+    void testActivityEmpty() {
+        AssertionError assertionError = assertThrows(AssertionError.class, () -> proPlannerPlus.getCompletionStatus(emptyProject));
+        Assertions.assertEquals("No activities exists in project", assertionError.getMessage());
+    }
+
+    @Test
+    void checkCompletionStatusPostConditions() throws OperationNotAllowedException {
+        proPlannerPlus.getCompletionStatus(project);
+    }
+
 }
